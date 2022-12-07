cf395352c18605e7a5d44e742572cd63afd803b4
이경범 <tkdwk567@naver.com>
2022-12-07 15:26:41 +0900

    [feat] 제한 사항 예외 처리

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -4,11 +4,22 @@ import onboarding.problem3.RuleValue;
 
 public class Problem3 {
     public static int solution(int number) {
+        if (!isLength(number)) {
+            throw new IllegalArgumentException();
+        }
+
         int count = 0;
         for (int gameTurn = 1; gameTurn <= number; gameTurn++) {
             count += RuleValue.clapCount(gameTurn);
         }
 
         return count;
+    }
+
+    private static boolean isLength(int number) {
+        if (number >= 1 && number <= 10000) {
+            return true;
+        }
+        return false;
     }
 }
