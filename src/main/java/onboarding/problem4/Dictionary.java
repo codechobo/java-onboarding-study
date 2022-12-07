dc1741fe627d95b229dc5df7d69677b97cd9c024
이경범 <tkdwk567@naver.com>
2022-12-07 22:33:39 +0900

    [refactor] 사전의 문자를 참고하는 기능과 번역하는 기능 refactor

--- src/main/java/onboarding/problem4/Dictionary.java
+++ src/main/java/onboarding/problem4/Dictionary.java
@@ -11,6 +11,7 @@ import java.util.stream.Stream;
 public class Dictionary {
 
     private final Map<Character, Character> dictionaryMap;
+    private StringBuilder sb = new StringBuilder();
 
     public Dictionary() {
         this.dictionaryMap = new ConcurrentHashMap<>();
@@ -22,5 +23,43 @@ public class Dictionary {
         for (char data = 'A'; data <= 'Z'; data++) {
             this.dictionaryMap.put(data, reverseData--);
         }
+    }
+
+    // 참고하다
+    public void refer(char data) {
+        if (Character.isLowerCase(data)) {
+            char key = Character.toUpperCase(data);
+            Character valueData = getValueData(key);
+
+            char value = Character.toLowerCase(valueData);
+
+            this.sb.append(value);
+            return;
+        }
+
+        if (Character.isUpperCase(data)) {
+            Character value = getValueData(data);
+
+            this.sb.append(value);
+            return;
+        }
+
+        if (data == ' ') {
+            this.sb.append(' ');
+        }
+
+    }
+
+    private Character getValueData(char key) {
+        if (this.dictionaryMap.containsKey(key)) {
+            return dictionaryMap.get(key);
+        }
+
+        throw new IllegalArgumentException("사전에 없는 문자입니다.");
+    }
+
+    // 번역하다.
+    public String translate() {
+        return this.sb.toString();
     }
 }
