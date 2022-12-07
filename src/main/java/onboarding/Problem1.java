82c35d8d2a882afa44ca66e4cf1ab6b96086befd
이경범 <tkdwk567@naver.com>
2022-12-07 13:22:08 +0900

    [refactor] 책을 임의로 펼친다.

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,24 +4,24 @@ import onboarding.problem1.infra.PageNumberAndSizeValidation;
 import onboarding.problem1.serivce.PageService;
 import onboarding.problem1.type.ResultValue;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class Problem1 {
 
-    private static final PageService pageService = new PageService();
-
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        try {
-            PageNumberAndSizeValidation validation = new PageNumberAndSizeValidation();
+        // 1. 책을 임의로 펼친다.
+        //2. 왼쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
+        //3. 오른쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
+        //4. 2~3 과정에서 가장 큰 수를 본인의 점수로 한다.
+        //5. 점수를 비교해 가장 높은 사람이 게임의 승자가 된다.
+        //6. 시작 면이나 마지막 면이 나오도록 책을 펼치지 않는다.
 
-            int pobiMaxValue = pageService.findMaxValue(pobi, validation);
-            int crongMaxValue = pageService.findMaxValue(crong, validation);
+        return 0;
+    }
 
-            return pageService.getResult(pobiMaxValue, crongMaxValue);
-        } catch (IllegalArgumentException e) {
-
-            System.out.println(e.getMessage());
-            return ResultValue.EXCEPTION_VALUE.getValue();
-        }
+    // 1. 책을 임의로 펼친다.
+    public static Integer pageOpen(List<Integer> person, int position) {
+        return person.get(position);
     }
 }
